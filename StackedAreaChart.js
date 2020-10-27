af7ae3495e164d604d861fb4d3cee904c6bedaac
Sung Joon Park <sungjoonpark0909@gmail.com>
2020-10-27 12:38:13 -0400

    Update StackedAreaChart.js
    minor changes

--- StackedAreaChart.js
+++ StackedAreaChart.js
@@ -156,7 +156,6 @@ export default function StackedAreaChart(container) {
                     selected = null;
                 } else {
                     selected = d.key;
-
                 }
                 update(data);
             });
